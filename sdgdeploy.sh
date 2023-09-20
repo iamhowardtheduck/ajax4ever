44abf72030581e074a950aea4c6eb2489187bf82
Peter Titov <peter.titov@gmail.com>
2023-09-20 09:32:50 -0400

    Update sdgdeploy.sh
    Mr. Clean?

--- sdgdeploy.sh
+++ sdgdeploy.sh
@@ -7,9 +7,9 @@ fi
 sudo apt update -y
 sudo apt install wget unzip openjdk-17-jre-headless git -y
 cd /home/$USER && git clone https://github.com/ajpahl1008/simple-data-generator.git
-cd /opt/ && sudo wget https://services.gradle.org/distributions/gradle-7.6.1-bin.zip && unzip gradle-7.6-bin.zip && sudo mv gradle-7.6.1 gradle
+cd /opt/ && sudo wget https://services.gradle.org/distributions/gradle-7.6.1-bin.zip && unzip gradle-7.6.1-bin.zip && sudo mv gradle-7.6.1 gradle
 echo "export GRADLE_HOME=/opt/gradle" | sudo tee -a /etc/profile.d/gradle.sh
-echo "export PATH=${GRADLE_HOME}/bin:${PATH}" | sudo tee -a /etc/profile.d/gradle.sh
+echo "export PATH=\${GRADLE_HOME}/bin:\${PATH}" | sudo tee -a /etc/profile.d/gradle.sh
 sudo chmod +x /etc/profile.d/gradle.sh
 source /etc/profile.d/gradle.sh
 echo "What is your Elasticsearch endpoint?  Just the host, not the 'https://' or port."
